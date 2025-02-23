3184f9396135352c0ef37965d905f75552267376
junichi komuta <komuta@q-co.jp>
2025-02-24 03:05:02 +0900

    rm: いらないコメントアウトを削除

--- src/dashboard/scoreboard/App.tsx
+++ src/dashboard/scoreboard/App.tsx
@@ -1,74 +1,10 @@
-import { Button, Stack } from '@mui/material';
-import { BattleManegement } from '../../components/BattleManegement';
-import { BattleToggleButton } from '../../components/BattleToggleButton';
-import { PokemonSelector } from '../../components/PokemonSelector';
-import { PokemonAvatar } from '../../components/PokemonSelector/PokemonAvatar';
-import { StatusAilmentBadge } from '../../components/PokemonSelector/StatusAilmentBadge';
-import { TerastallizeBadge } from '../../components/PokemonSelector/TerastallizeBadge';
-import { StatusAilmentSelector } from '../../components/StatusAilmentSelector';
-import { TerastalButton } from '../../components/Terastal/TerastalButton';
 import './App.css';
-import { useReplicant } from '../../hooks/useReplicant';
-import { Timer } from '../../components/Timer/Timer';
-import { TopcutSelector } from '../../components/Topcut/TopcutSelector';
-import DisplayToucut from '../../components/Topcut/ToucutPlayerDisplay';
+import { Stack } from '@mui/material';
 import { BattleManegements } from './BattleManegements';
 import { WrappingTimer } from './WrappingTimer';
 import { Infomations } from './Infomations';
-// import { Bestof } from '../../components/Bestof';
-// import { Round } from '../../components/Round';
-// import { Scores } from '../../components/Scores';
-// import { SwissScores } from '../../components/SwissScores';
-// // import { BattleToggleButton } from '../../components/BattleToggleButton';
-
-// import { TerastalButton } from '../../components/Terastal/TerastalButton';
-
-// import { TerastalTypeSelector } from '../../components/Terastal/TerastalTypeSelector';
-// import { StatusAilmentSelector } from '../../components/StatusAilmentSelector';
 
 export function App() {
-  // const [my, setMy] = useAtom(
-  //   battleStateAtomFamily({ playerSide: 'Player1', pokemonNum: 'pokemon1' })
-  // );
-
-  //   const [my2, setMy2] = useAtom(
-  //     battleStateAtomFamily({ playerSide: 'Player1', pokemonNum: 'pokemon2' })
-  //   );
-
-  //     const [my3, setMy3] = useAtom(
-  //       battleStateAtomFamily({ playerSide: 'Player2', pokemonNum: 'pokemon1' })
-  //     );
-
-  // const swissPlayer1 = useAtomValue(swissAtom({ playerSide: 'Player1' }));
-  // const swissPlayer2 = useAtomValue(swissAtom({ playerSide: 'Player2' }));
-
-  // const swissSwap = useSetAtom(swissSwapAtom);
-
-  // console.log('swissPlayer1', swissPlayer1);
-  // console.log('swissPlayer2', swissPlayer2);
-
-  // const Benched = () => {
-  //   setMy('Benched');
-  // };
-  // const Active = () => {
-  //   setMy('Active');
-  // };
-  // const Fainting = () => {
-  //   setMy('Fainting');
-  // };
-  // const swap = () => {
-  //   swissSwap();
-  // }
-
-  // const test = useAtomValue(testAtom({ playerSide: 'Player1', pokemonNum: 'pokemon1' }));
-  // console.log(test);
-
-  // const { Heading, WhiteCard } = useStyled();
-
-  // const ranking = () => nodecg.sendMessage('ranking');
-  // const onClick = () => {
-  //   ranking();
-  // };
   return (
     <>
       <Stack spacing={2}>
--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,46 +1,18 @@
-// import Database from 'better-sqlite3';
-// import { EntryMargeParty, EntryParty, EntryPokemon } from '../types/scoreboard';
-// import { KP, RankingContents, ConvertedRanking } from '../types/replicant';
-// import { pokedex } from './pokedex';
 import { timer } from './timer';
 import type { NodeCG } from './nodecg';
-
-// import { type } from '../../prisma/generated/pokedex';
 import { pokedex } from './pokedex';
 import { tournament } from './tournament';
 import { PlayerDatabase } from './tournament/PlayerDatabase';
 import { KPs } from './KPs';
 import { pokemonRanking } from './PokemonRanking';
 import { topcutKPs } from './TopcutKPs';
-// import { pokedexDb } from './prisma';
 
-// import { PrismaClient } from '@prisma/client';
-
-// type PartyType = readonly EntryParty[];
-// type PokemonType = readonly EntryPokemon[];
-// type KPType = readonly KP[];
-
-// type MargePartyType = readonly EntryMargeParty[];
-
-// // SQlite内では配列はstring型として格納されているので、取り出す際はitem~moveはstringとして定義する
-// type queryRanking = {
-//   id: number;
-//   pokemon: string;
-//   item: string;
-//   ability: string;
-//   teraType: string;
-//   move: string;
-// };
-// const pokedexDb = new Pokedex({
-//   datasources: { db: { url: 'file:../src/db/pokedex.db' } },
-// });
 export default (nodecg: NodeCG) => {
-
   // サーバー側にログを出す場合のコード
   // const log = new nodecg.Logger('pokedex');
   // log.info(repPokedex.value);
 
-  // nodecg.listenFor('types', types);
+
   PlayerDatabase(nodecg);
   tournament(nodecg);
   pokedex(nodecg)
@@ -48,114 +20,4 @@ export default (nodecg: NodeCG) => {
   KPs(nodecg);
   pokemonRanking(nodecg);
   topcutKPs(nodecg);
-  // pokedex(nodecg);
-  // // Pokedexをインポートするextension
-  // pokedex(nodecg);
-
-  // // サーバー側にログを出す場合のコード
-  // // const log = new nodecg.Logger('partyLog');
-  // // log.info(repParty.value);
-  // const repParty = nodecg.Replicant('Party');
-  // const repPokemon = nodecg.Replicant('Pokemon');
-  // const repRanking = nodecg.Replicant('Ranking');
-  // const repKP = nodecg.Replicant('KP');
-  // const repK2P = nodecg.Replicant('K2P');
-  // const repK3P = nodecg.Replicant('K3P');
-  // const repK4P = nodecg.Replicant('K4P');
-  // const repK5P = nodecg.Replicant('K5P');
-  // const repK6P = nodecg.Replicant('K6P');
-  // const repK2P_KPtop10 = nodecg.Replicant('K2P_KPtop10');
-
-  // const dbPath = './bundles/pokemon/src/db/tournament.db'; //rootからの相対パス
-  // const db = new Database(dbPath);
-  // const queryDataParty: PartyType = db
-  //   .prepare(
-  //     'SELECT id,accountID,player_name,pokemon1,pokemon2,pokemon3,pokemon4,pokemon5,pokemon6 FROM Party'
-  //   )
-  //   .all() as PartyType;
-
-  // const queryDataPokemon: PokemonType = db
-  //   .prepare(
-  //     'SELECT id,accountID,player_name,pokemon_name,type1,type2,teraType,ability,item,move1,move2,move3,move4 FROM Pokemon'
-  //   )
-  //   .all() as PokemonType;
-
-  // // PartyとPokemonのデータから、各パーティのポケモンのテラスタイプを引っ張ってきてひとつのデータにまとめる
-  // const result: MargePartyType = queryDataParty.map((list, i) => {
-  //   const players = queryDataPokemon.filter((player) => player.accountID === list.accountID);
-  //   return {
-  //     id: i + 1,
-  //     accountID: list.accountID,
-  //     player_name: list.player_name,
-  //     pokemon1: {
-  //       name: list.pokemon1,
-  //       teraType: players[0].teraType,
-  //     },
-  //     pokemon2: {
-  //       name: list.pokemon2,
-  //       teraType: players[1].teraType,
-  //     },
-  //     pokemon3: {
-  //       name: list.pokemon3,
-  //       teraType: players[2].teraType,
-  //     },
-  //     pokemon4: {
-  //       name: list.pokemon4,
-  //       teraType: players[3].teraType,
-  //     },
-  //     pokemon5: {
-  //       name: list.pokemon5,
-  //       teraType: players[4].teraType,
-  //     },
-  //     pokemon6: {
-  //       name: list.pokemon6,
-  //       teraType: players[5].teraType,
-  //     },
-  //   };
-  // });
-
-  // // KP情報を読み込む
-  // const queryDataKP: KPType = db.prepare('SELECT * FROM KP').all() as KPType;
-  // const queryDataK2P: KPType = db.prepare('SELECT * FROM K2P').all() as KPType;
-  // const queryDataK3P: KPType = db.prepare('SELECT * FROM K3P').all() as KPType;
-  // const queryDataK4P: KPType = db.prepare('SELECT * FROM K4P').all() as KPType;
-  // const queryDataK5P: KPType = db.prepare('SELECT * FROM K5P').all() as KPType;
-  // const queryDataK6P: KPType = db.prepare('SELECT * FROM K6P').all() as KPType;
-  // const queryDataK2P_KPtop10: KPType = db.prepare('SELECT * FROM K2P_KPtop10').all() as KPType;
-
-  // const queryDataRanking = db.prepare('SELECT * FROM Ranking').all() as queryRanking[];
-
-  // // Json形式でデータベースに保存したので、読み込む際もJsonとして読み込む
-  // const readJsonRanking: ConvertedRanking[] = queryDataRanking.map((value) => {
-  //   // 文字列がバイナリで保存されているので、JSON.perseで元に戻す
-  //   // console.log('value.move', value.move); ->
-  //   // [{ "Rank": 1.0, "Name": "\u304b\u3048\u3093\u307b\u3046\u3057\u3083", "Score": 100.0 }, { "Rank": 1.0, "Name": "\u304b\u307f\u304f\u3060\u304f", "Score": 100.0 },...}]
-
-  //   const itemRanking: RankingContents[] = JSON.parse(value.item);
-  //   const abilityRanking: RankingContents[] = JSON.parse(value.ability);
-  //   const teraTypeRanking: RankingContents[] = JSON.parse(value.teraType);
-  //   const moveRanking: RankingContents[] = JSON.parse(value.move);
-
-  //   return {
-  //     id: value.id,
-  //     pokemon: value.pokemon,
-  //     item: itemRanking,
-  //     ability: abilityRanking,
-  //     teraType: teraTypeRanking,
-  //     move: moveRanking,
-  //   };
-  // });
-
-  // // console.log(readJsonRanking);
-
-  // repParty.value = result;
-  // repPokemon.value = queryDataPokemon;
-  // repRanking.value = readJsonRanking;
-  // repKP.value = queryDataKP;
-  // repK2P.value = queryDataK2P;
-  // repK3P.value = queryDataK3P;
-  // repK4P.value = queryDataK4P;
-  // repK5P.value = queryDataK5P;
-  // repK6P.value = queryDataK6P;
-  // repK2P_KPtop10.value = queryDataK2P_KPtop10;
 };
