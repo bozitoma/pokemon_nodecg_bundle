197af2b54186e9f5087207e728c3553266aac3d6
junichi komuta <komuta@q-co.jp>
2025-02-24 03:05:20 +0900

    feat: extensionのデータベースの読み取り方法を調整

--- src/extension/prisma.ts
+++ src/extension/prisma.ts
@@ -1,11 +1,29 @@
-import { PrismaClient as Pokedex } from '@prisma/generated/pokedex';
-import { PrismaClient as Tournament } from '@prisma/generated/tournament';
+import { PrismaClient as Pokedex } from '../../prisma/generated/pokedex';
+import { PrismaClient as Tournament } from '../../prisma/generated/tournament';
+import { join } from 'path';
+
+// 現在のファイルからの相対パスで正しいDBファイルの場所を指定
+const DB_PATH = join(__dirname, '../src/db');
 
 export const pokedexDb = new Pokedex({
-  // @prisma/generated から見た相対パス
-  datasources: { db: { url: 'file:../../src/db/pokedex.db' } },
+  datasources: { 
+    db: { url: `file:${join(DB_PATH, 'pokedex.db')}` } 
+  }
 });
 
 export const tournamentDb = new Tournament({
-  datasources: { db: { url: 'file:../../src/db/tournament.db' } },
+  datasources: { 
+    db: { url: `file:${join(DB_PATH, 'tournament.db')}` } 
+  }
 });
+
+
+// 接続テスト
+pokedexDb.$connect()
+  .then(() => console.log('Pokedex DB connected successfully'))
+  .catch(e => console.error('Pokedex DB connection error:', e, DB_PATH));
+
+// 接続テスト
+tournamentDb.$connect()
+  .then(() => console.log('Tournament DB connected successfully'))
+  .catch(e => console.error('Tournament DB connection error:', e, DB_PATH));
